bbb28b6d8c7080aa22172471d26bdc5dcee048ed
Duy Nguyễn <nhdduy67@gmail.com>
2023-08-30 09:54:53 +0700

    Add data

--- Dataset/createBook.py
+++ Dataset/createBook.py
@@ -14,20 +14,23 @@ locations = ['Quận 1', 'Quận 2', 'Quận 3', 'Quận 4', 'Quận 5', 'Quận
 ky_han_options = ["Không kỳ hạn", "Kỳ hạn 3 tháng", "Kỳ hạn 6 tháng"]
 
 
-def generate_random_date(month):
-    """Generate a random date within the specified month of 2024."""
-    if month == 2:
-        start_date = datetime(2024, 2, 1)
-        # February in 2024 has 29 days (leap year)
-        end_date = datetime(2024, 2, 29)
-    elif month == 5:
-        start_date = datetime(2024, 5, 1)
-        end_date = datetime(2024, 5, 31)
-    elif month == 8:
-        start_date = datetime(2024, 8, 1)
-        end_date = datetime(2024, 8, 31)
+def generate_random_date(ky_han):
+    """Generate a random date based on the `ky_han` option."""
+    if ky_han == "Không kỳ hạn":
+        start_date = datetime(2024, 8, 25)
+        end_date = datetime(2024, 9, 5)
+    elif ky_han == "Kỳ hạn 3 tháng":
+        # Either February or May 2024
+        month = random.choice([2, 5])
+        start_date = datetime(2024, month, 1)
+        end_date = datetime(2024, month, 29 if month == 2 else 31)
+    elif ky_han == "Kỳ hạn 6 tháng":
+        # Either February or March 2024
+        month = random.choice([2, 3])
+        start_date = datetime(2024, month, 1)
+        end_date = datetime(2024, month, 29 if month == 2 else 31)
     else:
-        raise ValueError("Only months 2, 5, and 8 are supported.")
+        raise ValueError("Invalid `ky_han` option.")
 
     random_days = random.randint(0, (end_date - start_date).days)
     return start_date + timedelta(days=random_days)
@@ -47,21 +50,13 @@ existing_numbers = set()
 
 with open('createBook.txt', 'w', encoding='utf-8') as output_file:
     for i, member in enumerate(data):
-        # Cycle through the months 2, 5, 8 in each block of 200 accounts
-        if i % 200 < 67:  # First ~1/3 of 200 accounts
-            month = 2
-        elif i % 200 < 134:  # Second ~1/3 of 200 accounts
-            month = 5
-        else:  # Last ~1/3 of 200 accounts
-            month = 8
-
         # Alternate between the options for `ky_han`
         ky_han = random.choice(ky_han_options)
 
         full_name = member.get('full_name')
         unique_number = generate_unique_number(existing_numbers)
         location = random.choice(locations)
-        random_date = generate_random_date(month).strftime('%Y-%m-%d')
+        random_date = generate_random_date(ky_han).strftime('%Y-%m-%d')
         amount = random.randint(100001, 2000000)
         amount = round(amount, -3)
 
