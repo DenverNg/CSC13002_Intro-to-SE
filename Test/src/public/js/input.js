bce0b90d2074dc4a031b54b7a8857f1fa3ff7475
LuciaNgo <ngotrieuman16052004@gmail.com>
2024-08-13 14:08:12 +0700

    redesign the createbook form
    Need to do in next update:
    - combine with the verify form.
    - forms appear on transaction page (not routing).
    - fix the input component bugs (described in verify issue or contact Duy for more details).

--- Test/src/public/js/input.js
+++ Test/src/public/js/input.js
@@ -13,29 +13,44 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 })
 
-function inputClicked({id='', inputType='text'}) {
+document.addEventListener("DOMContentLoaded", () => {
+    const groups = document.querySelectorAll(".inp-createbook");
+    
+    groups.forEach(element => {
+        var input = element.firstElementChild;
+        var title = input.nextElementSibling;
+
+        input.addEventListener("blur", () => {
+            if (input.value == "") {
+                resetInput({input, title, fontSize: '20px', top: '30% + 1px', animation: 'inputTitleMoveDown_createbook'});
+            }
+        });
+    });
+})
+
+function inputClicked({id='', inputType='text', fontSize='17px', top='4%', animation='inputTitleMoveUp'}) {
     const form = document.getElementById(id);
     var input = form.firstElementChild;
     var title = input.nextElementSibling;
 
     input.focus();
-    title.style.animation = "inputTitleMoveUp ease 0.3s forwards";
+    title.style.animation = `${animation} ease 0.3s forwards`;
     input.type = inputType;
     title.addEventListener("animationend", () => {
-        title.style.fontSize = "17px";
-        title.style.top = "calc(4%)";
+        title.style.fontSize = fontSize;
+        title.style.top = `calc(${top})`;
         title.style.left = "15px";
         title.style.backgroundImage = "linear-gradient(0, #FFFF, #EEECFF)";
     }, { once: true });
 }
 
-function resetInput({input, title}) {
-    title.style.animation = `inputTitleMoveDown ease 0.3s forwards`;
+function resetInput({input, title, fontSize='28px', top='25% + 2px', animation='inputTitleMoveDown'}) {
+    title.style.animation = `${animation} ease 0.3s forwards`;
     input.type = "text";
     title.addEventListener("animationend", () => {
         title.style.animation = "none";
-        title.style.fontSize = "28px";
-        title.style.top = "calc(25% + 2px)";
+        title.style.fontSize = fontSize;
+        title.style.top = `calc(${top})`;
         title.style.left = "0";
         title.style.backgroundImage = "none";
     }, { once: true });
